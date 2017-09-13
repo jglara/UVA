9e9f38fe4317113fa26f0034a714384d47f636a1
jglara <jglara@gmail.com>
2017-09-13 12:53:03 +0200

    305: Passed

--- 305/305.cc
+++ 305/305.cc
@@ -2,9 +2,50 @@
 #include <vector>
 #include <string>
 
+class Joseph {
+  unsigned int N_;
+  unsigned int M_;
+  std::vector<bool> people_;
+  unsigned int index_;
+  unsigned int dead_;
+  
+public:
+  Joseph(unsigned int N,unsigned int M):
+    N_(N), M_(M),
+    people_(N,true), index_(N-1),
+    dead_(0)
+    { }
+
+  void count1() {
+    do {
+      index_ = (index_ + 1) % N_;
+    } while (!people_[index_]);
+  }
+
+  unsigned int killNext() {
+    unsigned int times(M_ % (N_ -dead_));
+    if (times == 0) {
+      times = N_ - dead_;
+    }
+
+    for (unsigned int i=0; i<times; i++) {
+      count1();
+    }
+    people_[index_] = false;
+    dead_++;
+
+    return index_;
+  }
+
+  unsigned int dead() const {return dead_;}
+
+};
+
 int main()
 {
 
+  std::vector<unsigned int> solutions(13+1,0);
+  
   std::string line;
   while (std::getline(std::cin, line)) {
     unsigned int K= atoi(line.c_str());
@@ -12,33 +53,29 @@ int main()
 
     if (K==0) {
       return 0;
+    } else if (solutions[K] != 0) {
+      std::cout << solutions[K] << "\n";
+      continue;
     }
 
-    for (unsigned int M=1;;++M) {
-      // std::cout << "\nTest " << M << ": ";
+    for (unsigned int M=K+1;;++M) {
+      //std::cout << "\nTest " << M << ": ";
       // test M
-      std::vector<bool> people(N,true);
-      unsigned int index((M-1)%N);
-      unsigned int dead(0);
-      while (index >= K && dead<K) {
-        //std::cout << " " << index;
-        people[index] = false;
-        ++dead;
-        
-        // find next guy to die
-        for (unsigned int i=0; i <M; i++) {
-          do {
-            index = (index + 1) % N;
-          } while(!people[index]);
-        }
-      }
-      if (dead == K) {
+      Joseph joseph(N,M);
+
+      unsigned int killed(0);
+      do {
+        killed = joseph.killNext();
+        //std::cout << " " << killed;
+        if (killed < K) break;
+      } while (joseph.dead() < K);
+
+      if (killed >= K) {
         std::cout << M << "\n";
+        solutions[K] = M;
         break;
       }
     }
-
-    
 
 
   }
