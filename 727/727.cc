da18de2fc27e73d9194591f85c024a4d5f48a371
jglara <jglara@gmail.com>
2017-09-23 19:20:30 +0200

    727: AC

---
+++ 727/727.cc
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include <stack>
+#include <unordered_map>
+
+using namespace std;
+
+unordered_map<char, int>precedence{
+  {'+',1},
+  {'-',1},
+  {'*',2},
+  {'/',2}};
+
+int main()
+{
+
+  unsigned int N;
+  cin >> N; cin.ignore(100, '\n');
+  cin.ignore(100, '\n'); // ignore first line
+  for (;N>0;--N) {
+    stack<char> oper;
+
+    for (char c=cin.get(); c!='\n' && !cin.eof(); c=cin.get()) {
+
+      switch (c) {
+      case '(':
+        oper.push(c);
+        break;
+
+      case ')':
+        while (oper.top() != '(') {
+          cout << oper.top(); oper.pop();
+        }
+        oper.pop(); // extract '('
+        break;
+
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+        if (oper.empty() || precedence[c] > precedence[oper.top()] )  {
+          oper.push(c);
+        } else {
+
+          while (!oper.empty() and precedence[c] <= precedence[oper.top()]) {
+            cout << oper.top();
+            oper.pop();
+          }
+          oper.push(c);
+        }
+        break;
+
+      default: // operand
+        cout << c;
+        break;
+
+      }
+      cin.get(); // read the '\n'
+    }
+
+    while (!oper.empty()) {
+      cout << oper.top();
+      oper.pop();
+    }
+    cout << "\n";
+
+    if (N>1) cout << "\n";
+  }
+  
+
+}
